1114bb108189a367ad021e57b22cfce66dea4dbb
jb1011 <demenet.jb@hotmail.fr>
2022-07-28 14:32:30 +0200

    responsive modal web3

--- src/components/ModalWeb3.js
+++ src/components/ModalWeb3.js
@@ -2,8 +2,11 @@ import React from 'react'
 import '../styles/modal-social.scss'
 import { motion } from "framer-motion"
 import { ethers } from 'ethers'
+import { useMediaQuery } from "react-responsive";
 
 function ModalWeb3({ account, toggleModal, ethPrice, gasPrice, balance, transactions }) {
+
+    const isMobile = useMediaQuery({ maxWidth: 890 })
     return (
         <motion.div
             initial={{ opacity: 0, scale: 0.5 }}
@@ -12,19 +15,19 @@ function ModalWeb3({ account, toggleModal, ethPrice, gasPrice, balance, transact
             className='layout'
             onClick={toggleModal}
         >
-            <div style={{ lineHeight: '0px' }} className='modal center-simple-col' onClick={(e) => e.stopPropagation()}>
+            <div style={{ lineHeight: '0px' }} className='modal-web3 center-simple-col' onClick={(e) => e.stopPropagation()}>
                 <h3>Quick Overview:</h3>
                 <p style={{ marginTop: '-20px', fontSize: '0.8rem' }}>powered by etherscan api.</p>
-                <div className='center-simple'>
+                <div className={isMobile ? 'center-simple-col' :'center-simple'}>
                     <p>ETH Price: <span style={{ color: '#fca311' }}>$ {ethPrice}</span></p>
                     <p>Current Gas Price: <span style={{ color: '#fca311' }}>{gasPrice} wei</span></p>
                 </div>
-                <div className='center-simple'>
+                <div className={isMobile ? 'center-simple-col' : 'center-simple'}>
                     <a style={{ textDecoration: 'none' }} href={`https://etherscan.io/address/${account}`} rel='noopener noreferrer' target='_blank'>
                         <p>Your Wallet Balance: <span style={{ color: '#fca311' }}>{ethers.utils.formatEther(balance).substring(0, 5)} eth</span></p>
                     </a>
                 </div>
-                <div className='center-simple'>
+                <div className={isMobile ? 'center-simple-col' : 'center-simple'}>
                     <p>Your Last Transactions:</p>
                     {transactions.message === 'OK' && Object.keys(transactions).length !== 0 ? (transactions?.result.reverse().slice(0, 5).map((transaction, index) => {
                         return (
