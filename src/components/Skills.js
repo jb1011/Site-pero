c08b7bd9bb0457be3c2901669d27f0e6285fc1a9
jb1011 <demenet.jb@hotmail.fr>
2022-07-29 19:12:51 +0200

    better responsivity

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,6 @@ import SkillsJson from '../store/skills.json'
 
 function Skills2() {
     const [isShown, setIsShown] = useState(-1);
-
     const isMobile = useMediaQuery({ maxWidth: 890 });
     const size = isMobile ? 70 : 100;
 
@@ -23,15 +22,16 @@ function Skills2() {
             <div style={{ paddingTop: '50px' }} />
             <h1 style={{ textAlign: 'center' }}>Skills</h1>
             <div className='center-simple'>
-            <div style={{ display: 'flex', flexWrap:'wrap', gap: 10 }}>
-                {SkillsJson.map((skill) => {
-                    return (
-                        <div style={{flex: '1 0 21%', display: 'flex', justifyContent: 'center'}} key={skill.id}>
-                            <img src={skill.src} alt={skill.alt} style={{borderRadius: skill.radius}} width={size} height={size} onMouseEnter={() => setIsShown(skill.id)} onMouseLeave={() => setIsShown(-1)} />
-                        </div>
-                    )
-                })}
-            </div>
+                <div style={{ display: 'flex', flexWrap: 'wrap', gap: 20 }}>
+                    {SkillsJson.map((skill) => {
+                        const styleFlex = skill.id % 2 === 0 ? 'end' : 'start'
+                        return (
+                            <div style={isMobile ? { flex: '1 0 33%', display: 'flex', justifyContent: styleFlex } : { flex: '1 0 21%', display: 'flex', justifyContent: 'center' }} key={skill.id}>
+                                <img src={skill.src} alt={skill.alt} style={{ borderRadius: skill.radius }} width={size} height={size} onMouseEnter={() => setIsShown(skill.id)} onMouseLeave={() => setIsShown(-1)} />
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
             {showSkillText()}
         </div>
