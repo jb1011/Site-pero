b0935d133a8440cf6d76ccbd85af738884f2a485
jb1011 <demenet.jb@hotmail.fr>
2022-12-13 12:56:50 +0100

    new exp

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,14 @@
 import React from 'react'
-import Avax from '../assets/avax.png'
 
 function About() {
     return (
         <div className='center-simple-col'>
             <h1>About me</h1>
             <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', padding: '20px', textAlign: 'center' }}>
-
-                <p>Hi! I am a front-end developer from Paris, France. I specialized in Web3 after I went to school 42 in Paris.
-                    I worked 6 months for a big decentralized exchange on
-                    <span>
-                        <span> Avalanche </span><img style={{verticalAlign: 'sub'}} alt='Avax' src={Avax} width={15} height={15} />
-                    </span> amongst other NFT projects.
-                </p>
                 <p>
-                    I am always happy and ready to work on the next crypto project.
-                    Feel free to reach me via email or linkedIn.
+                    Hi! I am a front-end developer from Paris, France. I have a serious passion for Web 3.0, decentralised finance.
+                    <br />Feel free to contact me on linkedin or my email.
                 </p>
-                <p>
-                    See you soon!
-                </p>
-
             </div>
         </div>
     )
--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -3,7 +3,9 @@ import '../styles/experiences.scss'
 import PNG from '../assets/png.png'
 import Dripperz from '../assets/Dripperz.png'
 import Farandole from '../assets/farandole.png'
+import Ondefy from '../assets/ondefy.png'
 import Hyro from '../assets/hyro.png'
+import Room from '../assets/room.png'
 import ModalExperience from './ModalExperience'
 import { useMediaQuery } from "react-responsive";
 
@@ -22,15 +24,17 @@ function Experiences() {
             <div style={{ paddingTop: '150px' }} />
             <h1 style={{ textAlign: 'center' }}>Experiences</h1>
             <div className={isMobile ? 'exp-small' : 'exp'}>
-                <img style={{borderRadius: '50%'}} className={isMobile ? 'img-small' : 'img'} onMouseEnter={() => setIsShown(1)} onMouseLeave={() => setIsShown(false)} onClick={() => { toggleModal(); setId(0) }} src={Dripperz} alt="Dripperz" />
+                <img className={isMobile ? 'img-small' : 'img'} onMouseEnter={() => setIsShown(1)} onMouseLeave={() => setIsShown(false)} onClick={() => { toggleModal(); setId(0) }} src={Dripperz} alt="Dripperz" />
                 <img className={isMobile ? 'img-small' : 'img'} onMouseEnter={() => setIsShown(1)} onMouseLeave={() => setIsShown(false)} onClick={() => { toggleModal(); setId(1) }} src={PNG} alt="PNG" />
                 <img className={isMobile ? 'img-small' : 'img'} onMouseEnter={() => setIsShown(1)} onMouseLeave={() => setIsShown(false)} onClick={() => { toggleModal(); setId(2) }} src={Farandole} alt="Farandole" />
+                <img className={isMobile ? 'img-small' : 'img'} onMouseEnter={() => setIsShown(1)} onMouseLeave={() => setIsShown(false)} onClick={() => { toggleModal(); setId(3) }} src={Ondefy} alt="Ondefy" />
+                <img className={isMobile ? 'img-small' : 'img'} onMouseEnter={() => setIsShown(1)} onMouseLeave={() => setIsShown(false)} onClick={() => { toggleModal(); setId(4) }} src={Room} alt="Room420" />            
             </div>
-            {isShown === 1 ? <p style={{ textAlign: 'center', height: '20px' }}>Click me.</p> : <p style={{ height: '20px' }}></p>}
+            {isShown === 1 ? <p style={{ textAlign: 'center', height: '20px', paddingTop: 50 }}>Click me.</p> : <p style={{ height: '70px' }}></p>}
             <div style={{ paddingTop: '150px' }} />
             <h3 style={{ textAlign: 'center' }}>Hackaton</h3>
             <div className="exp">
-                <img className={isMobile ? 'img-small' : 'img'} onMouseEnter={() => setIsShown(2)} onMouseLeave={() => setIsShown(false)} onClick={() => { toggleModal(); setId(3) }} src={Hyro} alt="Hyro" />
+                <img className={isMobile ? 'img-small' : 'img'} onMouseEnter={() => setIsShown(2)} onMouseLeave={() => setIsShown(false)} onClick={() => { toggleModal(); setId(5) }} src={Hyro} alt="Hyro" />
             </div>
             {isShown === 2 ? <p style={{ textAlign: 'center', height: '20px' }}>Click me.</p> : <p style={{ height: '20px' }}></p>}
         </div>
